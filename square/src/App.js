58dd96698e48a1f4777048a1228b6fe8c9700f7c
Lee Gainer <LG@Lees-MacBook-Pro.local>
2019-04-12 15:48:55 -0400

    Created style folder, moved css files, created square.scss

--- square/src/App.js
+++ square/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
-import "./App.css";
+import "./style/App.css";
+import Nav from "./components/nav";
+
+// App combines the various elements in 1 div
 
 class App extends Component {
   render() {
     return (
       <div>
+        {<Nav />}
         <h1>something</h1>
       </div>
     );
---
+++ square/src/components/nav.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class NavBar extends Component {
+  state = {};
+  render() {
+    return (
+      <div id="nav">
+        <p>I'm a nav</p>
+      </div>
+    );
+  }
+}
+
+export default NavBar;
